7da95a5266c844d9525039c4e1faa8f1dc0f3713
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-05 15:22:39 +0100

    Working on the test suite of the Workshop

--- index.test.js
+++ index.test.js
@@ -1,22 +1,41 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
+const { describe, it, expect, beforeAll, afterEach } = require("@jest/globals");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest");
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("GET /musicians endpoint", () => {
+  // Write your tests here
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+  test("testing if the GET request is successful and the status code is 200", async () => {
+    // Sends request to `/musicians` endpoint
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
 
+  test("testing the response data", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
 
+    expect(responseData.length).toBe(3);
 
-
-    
-})
+    expect(responseData).toEqual(
+      expect.arrayContaining(
+        responseData.map((obj) => {
+          expect.objectContaining({
+            id: obj.id,
+            name: obj.name,
+            instrument: obj.instrument,
+            bandId: obj.bandId,
+          });
+        })
+      )
+    );
+  });
+});
