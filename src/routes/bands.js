86eb7ebb34c0ccd3792f6298ce04adde31eeb03f
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-08 09:55:12 +0100

    added a comment

--- src/routes/bands.js
+++ src/routes/bands.js
@@ -12,6 +12,8 @@ router.get("/:id", async (req, res) => {
   const bandId = req.params.id;
   const currentBand = await Band.findByPk(bandId);
 
+  // include the Musician(s) that are part of that particular Band
+
   res.status(200).send(currentBand);
 });
 
